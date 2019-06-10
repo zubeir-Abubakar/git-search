78746ce25d7b86f87ba9837ee1b353ba0a5f59eb
zubeir-Abubakar <zubkayare@gmail.com>
2019-06-10 10:03:49 +0300

    css styling addition

--- src/test.ts
+++ src/test.ts
@@ -3,7 +3,7 @@
 import 'zone.js/dist/zone-testing';
 import { getTestBed } from '@angular/core/testing';
 import {
-  BrowserDynamic TestingModule,
+  BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
 } from '@angular/platform-browser-dynamic/testing';
 
