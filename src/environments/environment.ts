a3b909a4d76b9fa14d4cd5cf2cee2fbe55330aef
zubeir-Abubakar <zubkayare@gmail.com>
2019-06-10 09:30:53 +0300

    services indication

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://api.github.com/users/zubeir-Abubakar'
+  apiUrl: "https://api.github.com/users/zubeir-Abubakar",
+  key:"7150fa6d72df23f016bc06e5f49cabb1ee951f5c"
 };
 
